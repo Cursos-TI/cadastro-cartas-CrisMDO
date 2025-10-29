7b1f64edff05b6e72ebb1b6842e7423f7080f46c
Cristiano Oliveira <97488397+CrisMDO@users.noreply.github.com>
2025-10-29 02:03:47 +0000

    Add Entrada e saida de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,30 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  long long populacao;
+  double area;
+  double pib;
+  int np_turisticos;
 
   // Área para entrada de dados
+  printf("Insira o valor da população do país:");
+  scanf("%lld", &populacao);
 
+  printf("Insira o valor da Área:");
+  scanf("%lf", &area);
+
+  printf("Insira o valor do PIB:");
+  scanf("%lf", &pib);
+
+  printf("Insira o numeros de pontos turisticos:");
+  scanf("%d", &np_turisticos);
+
+  
   // Área para exibição dos dados da cidade
-
+  printf("\n--- Valores Registrados ---\n");
+    printf("População: %lld\n", populacao);
+    printf("Área (km²): %.2f\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos Turísticos: %d\n", np_turisticos);
 return 0;
 } 
